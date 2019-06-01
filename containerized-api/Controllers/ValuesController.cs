8e2db224b017d146ad274fd77aaf232902877fa4
Bruno Fernando <bruno.feabreu@gmail.com>
2019-05-31 21:51:37 -0300

    Finished application

---
+++ containerized-api/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace containerized_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<String> Get()
+        {
+            return "Container running on " + System.Environment.MachineName;
+        }
+
+        [HttpPost]
+        public ActionResult<String> Post(string name)
+        {
+            return "Post made by " + name.ToString();
+        }
+    }
+}
